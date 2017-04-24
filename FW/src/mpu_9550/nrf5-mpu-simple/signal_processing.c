4293927f8a5f3e05dda06c555c0d476c133bc427
Sami Mahamoed <sami.mahamoed@aalto.fi>
2017-04-23 19:55:35 -0700

    test file update

--- FW/src/mpu_9550/nrf5-mpu-simple/signal_processing.c
+++ FW/src/mpu_9550/nrf5-mpu-simple/signal_processing.c
@@ -2,12 +2,19 @@
 #include "SEGGER_RTT.h"
 #include <math.h>
 
+//#define ACCEL
+#ifdef ACCEL
 #define MIN_TH 1e-3
 #define MAX_TH 6e-3
+#else
+#define MIN_TH 2
+#define MAX_TH 150
+#endif
 #define MAX_SLOP_SCAN 6
 #define MIN_SPECING 40
 #define MAX_SPECING 255
- 
+
+
 typedef struct{
   double magnitude;
 	double index;
@@ -15,11 +22,14 @@ typedef struct{
 }pick_t;
 
 uint8_t bpm;
-float buff_x[SAMPLE_SIZE];
-float buff_y[SAMPLE_SIZE];
-float buff_z[SAMPLE_SIZE];
-gyro_values_t accel_values;
-
+double buff_x[SAMPLE_SIZE];
+double buff_y[SAMPLE_SIZE];
+double buff_z[SAMPLE_SIZE];
+#ifdef ACCEL
+accel_values_t accel_values;
+#else
+gyro_values_t gyro_values;
+#endif
 /**@brief function initialize module parameters
  */
 void sig_init(void){
@@ -32,11 +42,18 @@ void sig_init(void){
  */
 void sig_read_bpm(uint16_t tick){
 	
-	mpu_read_gyro(&accel_values);
-	buff_x[tick] = ((float)accel_values.x);///16384.0f;
-	buff_y[tick] = ((float)accel_values.y);///16384.0f;
-	buff_z[tick] = ((float)accel_values.z);///16384.0f;
-	
+	//mpu_read_accel(&accel_values);
+	#ifdef ACCEL
+	mpu_read_accel(&accel_values);
+	buff_x[tick] = ((float)accel_values.x)/16384.0f;
+	buff_y[tick] = ((float)accel_values.y)/16384.0f;
+	buff_z[tick] = ((float)accel_values.z)/16384.0f;
+	#else
+	mpu_read_gyro(&gyro_values);
+	buff_x[tick] = ((float)gyro_values.x);
+	buff_y[tick] = ((float)gyro_values.y);
+	buff_z[tick] = ((float)gyro_values.z);
+	#endif
 
 }
 
@@ -61,6 +78,80 @@ void sig_read_bpm(uint16_t tick){
 			 }
 }
 */
+
+/*
+void sig_butterworth_filter_0_66_to_2_5(double*data, double *result)
+{
+  //memcpy(result,data,400U *sizeof(result));
+	//memset(&result,0,sizeof(result));
+
+	double * x, * y;
+	x = data;y = result;
+	for(int n=0;n<4;n++)y[n]=(double)x[n];
+	for(int n=4;n<SAMPLE_SIZE;n++){
+		y[n] = (  1 * x[n- 4])
+				 + (  0 * x[n- 3])
+				 + ( -2 * x[n- 2])
+				 + (  0 * x[n- 1])
+				 + (  1 * x[n- 0])
+
+				 + ( -0.9215032056 * y[n- 4])
+				 + (  3.7582371567 * y[n- 3])
+				 + ( -5.7518393940 * y[n- 2])
+				 + (  3.9151028964 * y[n- 1]);
+			 }
+	
+	
+}*/
+
+#define _1ST_STAGE_FILTER_4_TO_11
+#define _2ND_STAGE_FILTER_0_66_TO_2_5
+
+#ifdef _1ST_STAGE_FILTER_4_TO_11
+// Referenced http://www-users.cs.york.ac.uk/~fisher/mkfilter/
+void sig_butterworth_filter_4_to_11(double *data, double *result)
+{
+	double* x;
+	double* y;
+	x = data;y = result;
+	for(int n=0;n<4;n++)y[n]=(double)x[n];
+	for(int n=4;n<SAMPLE_SIZE;n++){
+		 y[n] = (  1 * x[n- 4])
+     + (  0 * x[n- 3])
+     + ( -2 * x[n- 2])
+     + (  0 * x[n- 1])
+     + (  1 * x[n- 0])
+
+     + ( -0.4504454301 * y[n- 4])
+     + (  2.0313852278 * y[n- 3])
+     + ( -3.6679952325 * y[n- 2])
+     + (  3.0818060650 * y[n- 1]);
+		 
+			 }
+}
+#elif defined(_1ST_STAGE_FILTER4_TO_22)
+void sig_butterworth_filter_4_to_22(double *data, double *result)
+{
+	double* x;
+	double* y;
+	x = data;y = result;
+	for(int n=0;n<4;n++)y[n]=(double)x[n];
+	for(int n=4;n<SAMPLE_SIZE;n++){
+		 y[n] = (  1 * (double)x[n- 4])     
+					+ (  0 * (double)x[n- 3])     
+					+ ( -2 * (double)x[n- 2])     
+					+ (  0 * (double)x[n- 1])
+					+ (  1 * (double)x[n- 0])
+
+				 + ( -0.7327260304 * y[n- 4])
+				 + (  3.0878833813 * y[n- 3])
+				 + ( -4.9675926050 * y[n- 2])
+				 + (  3.6108197545 * y[n- 1]);
+			
+			 }
+}
+
+#elif defined(_1ST_STAGE_FILTER_MATLAB_4_TO_11)
 void sig_butterworth_filter_4_to_11(float *X, double *Y)
 {
 	int j,k;
@@ -91,6 +182,35 @@ void sig_butterworth_filter_4_to_11(float *X, double *Y)
     Y[j] = dbuffer[0];
   }
 }
+#endif
+
+
+#ifdef _2ND_STAGE_FILTER_0_66_TO_2_5
+void sig_butterworth_filter_0_66_to_2_5(double*data, double *result)
+{
+  //memcpy(result,data,400U *sizeof(result));
+	//memset(&result,0,sizeof(result));
+
+	double * x, * y;
+	x = data;y = result;
+	for(int n=0;n<4;n++)y[n]=(double)x[n];
+	for(int n=4;n<SAMPLE_SIZE;n++){
+		y[n] = (  1 * x[n- 4])
+				 + (  0 * x[n- 3])
+				 + ( -2 * x[n- 2])
+				 + (  0 * x[n- 1])
+				 + (  1 * x[n- 0])
+
+				 + ( -0.9215032056 * y[n- 4])
+				 + (  3.7582371567 * y[n- 3])
+				 + ( -5.7518393940 * y[n- 2])
+				 + (  3.9151028964 * y[n- 1]);
+			 }
+	
+	
+}
+
+#elif defined(_2ND_STAGE_FILTER_MATLAB_0_66_TO_2_5)
 void sig_butterworth_filter_0_66_to_2_5(double*X, double *Y)
 {
 	int k,j;
@@ -122,30 +242,10 @@ void sig_butterworth_filter_0_66_to_2_5(double*X, double *Y)
     Y[j] = dbuffer[0];
   }
 }
-/*
-void sig_butterworth_filter_0_66_to_2_5(double*data, double *result)
-{
-  //memcpy(result,data,400U *sizeof(result));
-	//memset(&result,0,sizeof(result));
+#endif
 
-	double * x, * y;
-	x = data;y = result;
-	for(int n=0;n<4;n++)y[n]=(double)x[n];
-	for(int n=4;n<SAMPLE_SIZE;n++){
-		y[n] = (  1 * x[n- 4])
-				 + (  0 * x[n- 3])
-				 + ( -2 * x[n- 2])
-				 + (  0 * x[n- 1])
-				 + (  1 * x[n- 0])
 
-				 + ( -0.9215032056 * y[n- 4])
-				 + (  3.7582371567 * y[n- 3])
-				 + ( -5.7518393940 * y[n- 2])
-				 + (  3.9151028964 * y[n- 1]);
-			 }
-	
-	
-}*/
+
 void sig_dot_x_acceleration(double *x, double *R){
   int ii = 0;  
 	for(ii = 0; ii < SAMPLE_SIZE ; ii++)
@@ -158,12 +258,7 @@ void sig_resultant_acceleration(double *R){
 	 R[ii] = sqrt(R[ii]);
 	}
 } 
-void sig_resultant(double*z,double*wy,double*R){
-	int ii = 0;  
-	double a=1,w=1;
-	for(ii = 0; ii < SAMPLE_SIZE ; ii++)
-	 R[ii] = sqrt(a*pow(z[ii],2) + w*pow(wy[ii],2) );
-}
+
 
 double sig_get_max_th(pick_t *picks, uint8_t len){
 	
@@ -200,6 +295,15 @@ int ii;
 	}
 }
 
+double sig_get_mean(double* vector,uint16_t SIZE)
+{
+    uint16_t cc;
+    long double result=0;
+
+    for(cc = 0;cc<SIZE;cc++)
+        result += *(vector+cc);
+    return  result/SIZE;
+}
 
 uint8_t sig_calculate_bpm(uint8_t* ad_data){
 	
@@ -210,15 +314,42 @@ uint8_t sig_calculate_bpm(uint8_t* ad_data){
   memset(R,0,sizeof(R));
 	
 	
+	double mean = sig_get_mean(buff_x,SAMPLE_SIZE);
+	for(ii=0;ii < SAMPLE_SIZE; ii++)
+		buff_x[ii] = buff_x[ii]- mean;
+	#if defined( _1ST_STAGE_FILTER_4_TO_11) || defined(_1ST_STAGE_FILTER_MATLAB_4_TO_11)
   sig_butterworth_filter_4_to_11(buff_x, Xf);
+	#elif defined( _1ST_STAGE_FILTER_4_TO_22)
+	sig_butterworth_filter_4_to_22(buff_x, Xf);
+	#else
+	#error "choose filter " + __LINE__
+	#endif
 	sig_dot_x_acceleration(Xf,R);
-
   memset(Xf,0,sizeof(Xf));
-	sig_butterworth_filter_4_to_11(buff_y, Xf);
+	
+	mean = sig_get_mean(buff_y,SAMPLE_SIZE);
+	for(ii=0;ii < SAMPLE_SIZE; ii++)
+		buff_y[ii] = buff_y[ii]- mean;
+	#if defined( _1ST_STAGE_FILTER_4_TO_11) || defined(_1ST_STAGE_FILTER_MATLAB_4_TO_11)
+  sig_butterworth_filter_4_to_11(buff_y, Xf);
+	#elif defined( _1ST_STAGE_FILTER_4_TO_22)
+	sig_butterworth_filter_4_to_22(buff_y, Xf);
+	#else
+	#error "choose filter " + __LINE__
+	#endif
 	sig_dot_x_acceleration(Xf,R);
-
   memset(Xf,0,sizeof(Xf));
-	sig_butterworth_filter_4_to_11(buff_z, Xf);
+	
+	mean = sig_get_mean(buff_z,SAMPLE_SIZE);
+	for(ii=0;ii < SAMPLE_SIZE; ii++)
+		buff_z[ii] = buff_z[ii]- mean;
+	#if defined( _1ST_STAGE_FILTER_4_TO_11) || defined(_1ST_STAGE_FILTER_MATLAB_4_TO_11)
+  sig_butterworth_filter_4_to_11(buff_z, Xf);
+	#elif defined( _1ST_STAGE_FILTER_4_TO_22)
+	sig_butterworth_filter_4_to_22(buff_z, Xf);
+	#else
+	#error "choose filter " + __LINE__
+	#endif
 	sig_dot_x_acceleration(Xf,R);
 
 	sig_resultant_acceleration(R);
@@ -226,7 +357,6 @@ uint8_t sig_calculate_bpm(uint8_t* ad_data){
 
 	memset(Xf,0,sizeof(Xf));
 	double * filtered_signal = Xf;
-
 	sig_butterworth_filter_0_66_to_2_5(R, filtered_signal);
   sig_print(filtered_signal, SAMPLE_SIZE);
 
@@ -236,12 +366,17 @@ uint8_t sig_calculate_bpm(uint8_t* ad_data){
 	int16_t kk = 0;
 	int8_t slop[SAMPLE_SIZE];
 	memset(slop,0,sizeof(slop));
-	double val_previous = filtered_signal[0], val_current = 0;
+	double val_previous = filtered_signal[0], val_current = 0, previous_index = 0;
  
 	for(ii = 0; ii < SAMPLE_SIZE; ii++){
 		 if (filtered_signal[ii] <  MIN_TH || filtered_signal[ii] >  MAX_TH ) continue;
-    
-		 val_current = filtered_signal[ii];
+     if ((ii - previous_index)!=1){
+		      previous_index = ii;
+			    val_previous   = filtered_signal[ii]; 
+			    continue;
+		 }
+		 previous_index = ii;
+		 val_current 		= filtered_signal[ii];
 	  
 		 slop[ii] = ((val_current-val_previous)>0)?1:(((val_current-val_previous)<0)?-1:0);
     
@@ -266,21 +401,24 @@ uint8_t sig_calculate_bpm(uint8_t* ad_data){
 	
 	
 	
-	uint16_t cnt_slop = 0;
+
 	for(ii = 0; ii < kk; ii++){
+		  uint16_t cnt_slop_m = 0;
+			uint16_t cnt_slop_p = 0;
 			uint16_t index = pick[ii].index;
 			int jj;
-			for(jj = 0; jj < MAX_SLOP_SCAN; jj++)   
-					if(index + jj < SAMPLE_SIZE)if(slop[index+jj] > 0)cnt_slop++;
+			for(jj = 0; jj < MAX_SLOP_SCAN; jj++){   
+					if(index + jj < SAMPLE_SIZE)if(slop[index+jj] > 0)cnt_slop_m++;
+					if(index - jj >= 0)if(slop[index-jj] < 0)cnt_slop_p++;
+			}
 							
-			if(cnt_slop  > 1){
+			if(cnt_slop_m  > 3 || cnt_slop_p  > 3){
 				 for(jj = ii; jj < kk-1; jj++){
 						 pick[jj].index = pick[jj+1].index ;
 						 pick[jj].magnitude =  pick[jj+1].magnitude;
 						 pick[jj].type =  pick[jj].type;
 				 }
 						 kk = jj-1;
-						 cnt_slop = 0;
 			 }
 	}
 							 
--- algorithm_test/testing.m
+++ algorithm_test/testing.m
@@ -1,6 +1,6 @@
 
 
-    A1 = csvread('testdata6.csv');
+    A1 = csvread('testdata4.csv');
 %    A1 = coeff * csvread('accelerometer_reading_for_about_1min_18sec_in_50ms_interval_device_status_on_chest.csv',1);
 
     
@@ -102,21 +102,31 @@
     peak_index(1:11) = 0;
     peak_type(1:11) = 0;
  
-    max_th = 6e-3 ;
-    min_th = 1e-3;
-	
+    max_th = 100;
+    min_th = 2;
+	min_spacing = 50;
+     
 	kk = 1;
    
 	slop(1:L+1)= 0;
 
 	val_previous = Rf(1);
     val_current = 0;
- 
-        for ii = 1:L
-           
-        if Rf(ii) <  min_th || Rf(ii) >  max_th
+    previous_index = 1;
+    
+        for ii = 2:L
+        
+        if Rf(ii) <  min_th || Rf(ii) >  max_th    
             continue;
         end
+        
+        if ii - previous_index ~= 1
+            previous_index = ii;
+            val_previous = Rf(ii);
+            continue;
+        end
+        
+        previous_index = ii;
 		val_current = Rf(ii);
 	  
         
@@ -127,25 +137,26 @@
         else
             slop(ii) = 0;
         end
-    
-        if(slop(ii)==0 || slop(ii)*slop(ii-1) < 0)
+    fprintf('ch : ii %d ,slop(ii) = %d,slop(ii-1) = %d kk = %d   amp = %5f, T = %5f \n',ii,slop(ii),slop(ii-1) , kk , peak_magnitude(kk), ii*dt)  
+        if((slop(ii)==0 || slop(ii) < 0) && slop(ii-1) > 0)
         if(ii == 1) 
         continue;
         end
-       
+         
         peak_index(kk) = ii;
 
         if(slop(ii-1) > 0)
          peak_magnitude(kk) = val_current;
          peak_type(kk) = 1;
-        elseif(slop(ii-1) < 0)  %not happening
+        elseif(slop(ii-1) < 0)  
           peak_magnitude(kk) = val_current;
           peak_type(kk) = -1;
-           fprintf('wtf : ii %d, kk = %d   amp = %5f, T = %5f \n',ii,kk , peak_magnitude(kk), ii*dt)
+           fprintf('wtf : ii %d ,slop(ii) = %d,slop(ii-1) = %d kk = %d   amp = %5f, T = %5f \n',ii,slop(ii),slop(ii-1) , kk , peak_magnitude(kk), ii*dt)
         else
             continue;
         end
-         fprintf('ii %d, kk = %d   amp = %5f, T = %5f \n',ii,kk , peak_magnitude(kk), ii*dt)
+        
+         fprintf('#ii %d, kk = %d   amp = %5f, T = %5f, type = %d\n',ii,kk , peak_magnitude(kk), ii*dt,  peak_type(kk))
         kk = kk + 1;
         if(kk > 6)
         break;
@@ -154,37 +165,59 @@
         end
 		
 		val_previous = val_current;
+        
         end
 	
         
         
-        cnt_slop = 0;
-    
+        cnt_slop_m = 0;
+        cnt_slop_p = 0;
+        obs_m = 0;
+        obs_p = 0;
         peak_prob = 0;
-        
+        window = 6
+        prob_limit = window - window/3;
         for ii = 1:kk
+            cnt_slop_m = 0;
+            cnt_slop_p = 0;
+            obs_m = 0;
+            obs_p = 0;
             index = peak_index(ii)
-            for jj = 1:6   
+            
+            for jj = 1:window   
                 if(index + jj < L)
+                     obs_m = obs_m+1;
+                     fprintf('index #ii %d, slop = %d \n',jj, slop(index+jj))
                     if(slop(index+jj) > 0)
-                        cnt_slop = cnt_slop + 1;
+                        cnt_slop_m = cnt_slop_m + 1;
+                    end
+                end
+                
+                if(index - jj > 1)
+                     obs_p = obs_p+1;
+                     fprintf('index #ii %d, slop = %d \n',jj, slop(index-jj))
+                    if(slop(index-jj) < 0)
+                        cnt_slop_p = cnt_slop_p + 1;
                     end
                 end
             end
-            fprintf('cnt slop : %d\n',cnt_slop)
-            if(cnt_slop  > 1)
-               for jj = ii:kk-1
+            fprintf('cnt slop m : %d, cnt slop p: %d \n', cnt_slop_m, cnt_slop_p)
+            fprintf('filter #ii %d, index = %d ,  amp = %5f, T = %5f \n',ii, index , peak_magnitude(ii), index*dt)
+            fprintf('forward projection #peak_magnitude(jj) %d, peak_magnitude(jj+1) = %d \n',peak_magnitude(ii), peak_magnitude(ii+1))
+            
+            if(cnt_slop_m  > 3 || cnt_slop_p  > 3 || ((peak_magnitude(ii) <  peak_magnitude(ii+1))&&((peak_index(ii+1) - peak_index(ii)) < min_spacing )))
+               for jj = ii:kk
                    fprintf('filtring: jj %d, kk = %d   amp = %5f, T = %5f \n',jj,kk , peak_magnitude(jj), peak_index(jj)*dt)
                    peak_index(jj) = peak_index(jj+1) ;
                    peak_magnitude(jj) =  peak_magnitude(jj+1);
                    peak_type(jj) =  peak_type(jj+1);
                end
                    kk = jj-1
-                   cnt_slop = 0;
+                   
             end
         end
 	
-	for ii = 1:kk
+	for ii = 1:kk-1
         fprintf('filtered : ii %d, kk = %d   amp = %5f, T = %5f \n',ii,kk , peak_magnitude(ii), peak_index(ii)*dt)
     end
 	
@@ -192,35 +225,29 @@
     previous_maxima = -1;
     current_minima = 0;
     previous_minima = -1;
-    previous = -1;
     count_diff = 1;
-    min_spacing = 40;
+
     
     difference(1:11) = 0;
-
-	 
 	  
 		
-	    for ii = 1:kk
+	    for ii = 1:kk-1
+            
         if(peak_type(ii) == 1)
            if(previous_maxima == -1)
                previous_maxima = ii;
                current_maxima = ii;
-               if(previous == -1)
-				previous  = ii;
-               end
-            continue;
-               
+               continue;  
            end
            
            current_maxima  = ii;
            
-           if((peak_index(current_maxima) - peak_index(previous)) < min_spacing )
-                
-               if(peak_type(previous) ==1 && peak_magnitude(current_maxima) > peak_magnitude(previous) && difference(count_diff-1))
-                    difference(count_diff-1) = difference(count_diff-1) + peak_index(current_maxima) - peak_index(previous);
+           if((peak_index(current_maxima) - peak_index(previous_maxima)) < min_spacing )
+                 fprintf('filtered : peak_index(current_maxima) %d, peak_index(previous_maxima) = %d   peak_type(current_maxima) = %d \n',peak_index(current_maxima),peak_index(previous_maxima) ,peak_type(current_maxima))
+               if(peak_type(current_maxima) == 1 && peak_magnitude(current_maxima) > peak_magnitude(previous_maxima) && difference(count_diff-1))
+                    difference(count_diff-1) = difference(count_diff-1) + peak_index(current_maxima) - peak_index(previous_maxima);
+                      fprintf('filtered : difference(count_diff-1) %d\n',difference(count_diff-1))
                     previous_maxima = current_maxima;
-                    previous  = current_maxima;
                end              
              continue;
           end
@@ -229,7 +256,7 @@
            count_diff = count_diff + 1;
             
            previous_maxima = current_maxima;
-           previous  = current_maxima;
+      
            
           
         else
